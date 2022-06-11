30ddd8d69f7a1afffb57a8a01ab9592bde2cf96f
zkffhtm6523 <zkffhtm6523@gmail.com>
2022-06-12 00:12:43 +0900

    Feat : Kotlin-Kafka Project Comment : - Bootstrap server 추가 Issue : D21-79

--- src/main/kotlin/com/kafka/kotlin/configuration/KafkaTopicConfiguration.kt
+++ src/main/kotlin/com/kafka/kotlin/configuration/KafkaTopicConfiguration.kt
@@ -1,12 +1,26 @@
 package com.kafka.kotlin.configuration
 
+import org.apache.kafka.clients.admin.AdminClientConfig
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.config.TopicBuilder
 import org.springframework.kafka.core.KafkaAdmin
 
+
 @Configuration
 class KafkaTopicConfiguration {
+
+    @Value("\${spring.kafka.bootstrap-servers}")
+    lateinit var bootstrapServer: String
+
+    @Bean
+    fun kafkaAdmin(): KafkaAdmin? {
+        val configs: MutableMap<String, Any> = HashMap()
+        configs[AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrapServer
+        return KafkaAdmin(configs)
+    }
+
 
     // 토픽 빌드 및 신규토픽 생성
     @Bean
